5dbc70905fdac3833639554e3b0c0610ed45fe05
Diego Resendiz Ojeda <di_564@hotmail.com>
2023-07-19 11:52:02 -0700

    cambio fluent api

--- Course.Api/DataAccess/ApplicationDbContext.cs
+++ Course.Api/DataAccess/ApplicationDbContext.cs
@@ -15,4 +15,16 @@ public class ApplicationDbContext : DbContext
     public DbSet<Student> Students { get; set; }
     public DbSet<StudentCourse> StudentCourses { get; set; }
     public DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Teacher>().Property(v => v.Name)
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Course>().Property(e => e.Name)
+            .HasMaxLength(50);
+
+    }
 }
